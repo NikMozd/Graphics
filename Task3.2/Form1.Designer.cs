cc3326fbfc382ebf8ceeefb91da62da927dad8e6
Николай Моздоров <mozdorov@yandex.ru>
2018-10-01 21:56:22 +0300

    Task 3.2 is done

---
+++ Task3.2/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Task3._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(860, 520);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(913, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Имя файла";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(960, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(953, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Начальная точка";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(940, 244);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(39, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1018, 244);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(39, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(957, 292);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Цвет границы";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(940, 308);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(117, 69);
+            this.listBox1.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(925, 408);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "НАРИСОВАТЬ ГРАНИЦУ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1128, 544);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Task3.2/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task3._2
+{
+    public partial class Form1 : Form
+    {
+        byte[] rgbValues;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] colors = { "Красный", "Чёрный", "Синий", "Зелёный", "Белый" };
+            listBox1.Items.AddRange(colors);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        Tuple<int, int> find_next(Tuple<int, int> dot, int dir)
+        {
+            int x = dot.Item1;
+            int y = dot.Item2;
+            if (1 <= dir && dir <= 3)
+                ++y;
+            if (3 <= dir && dir <= 5)
+                --x;
+            if (5 <= dir && dir <= 7)
+                --y;
+            if (dir == 7 || dir == 0 || dir == 1)
+                ++x;
+            return new Tuple<int, int>(x, y);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Bitmap.FromFile(label1.Text);
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            Bitmap bmp = pictureBox1.Image as Bitmap;
+
+            // Lock the bitmap's bits.  
+            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            System.Drawing.Imaging.BitmapData bmpData =
+                bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                bmp.PixelFormat);
+
+            // Get the address of the first line.
+            IntPtr ptr = bmpData.Scan0;
+
+            // Declare an array to hold the bytes of the bitmap.
+            int bytes = Math.Abs(bmpData.Stride) * bmp.Height;
+            rgbValues = new byte[bytes];
+
+            // Copy the RGB values into the array.
+            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+
+            // Находим настоящий старт
+            var x = int.Parse(textBox1.Text) * bmpData.Width / pictureBox1.Width;
+            var y = int.Parse(textBox2.Text) * bmpData.Height / pictureBox1.Height;
+            var color = Color.FromArgb(rgbValues[(bmpData.Stride * y + 3 * x) + 2],
+                rgbValues[bmpData.Stride * y + 3 * x + 1],
+                rgbValues[bmpData.Stride * y + 3 * x]);
+            do
+            {
+                ++x;
+            } while (color == Color.FromArgb(rgbValues[bmpData.Stride * y + 3 * x + 2],
+                rgbValues[bmpData.Stride * y + 3 * x + 1],
+                rgbValues[bmpData.Stride * y + 3 * x]));
+
+            // Начинаем выделять границу
+            LinkedList<Tuple<int, int>> border = new LinkedList<Tuple<int, int>>();
+            var p = new Tuple<int, int>(x, y);
+            var first = border.AddLast(p);
+            var last = first;
+
+            // Вторая точка границы
+            // 0 - вниз, далее - против часовой стрелки
+            int direction = 0;
+            for (; ; ++direction)
+            {
+                if (direction == 8)
+                    return;
+                var next = find_next(last.Value, direction);
+                int xx = next.Item1;
+                int yy = next.Item2;
+                Color clr = Color.FromArgb(rgbValues[bmpData.Stride * yy + 3 * xx + 2],
+                    rgbValues[bmpData.Stride * yy + 3 * xx + 1],
+                    rgbValues[bmpData.Stride * yy + 3 * xx]);
+                if (clr == color)
+                {
+                    p = new Tuple<int, int>(xx, yy);
+                    last = border.AddLast(p);
+                    break;
+                }
+            }
+
+            // Все остальные точки
+            //while (first.Value != last.Value)
+            for (int k = 0; k < 10000; ++k)
+            {
+                int nd = (direction + 6) % 8;
+                for (; ; ++nd)
+                {
+                    if (nd - 8 == (direction + 6) % 8)
+                        return;
+                    var new_dir = nd % 8;
+                    if (nd == (direction + 4) % 8)
+                        continue;
+                    var next = find_next(last.Value, new_dir);
+                    int xx = next.Item1;
+                    int yy = next.Item2;
+                    Color clr = Color.FromArgb(rgbValues[bmpData.Stride * yy + 3 * xx + 2],
+                        rgbValues[bmpData.Stride * yy + 3 * xx + 1],
+                        rgbValues[bmpData.Stride * yy + 3 * xx]);
+                    if (clr == color)
+                    {
+                        p = new Tuple<int, int>(xx, yy);
+                        last = border.AddLast(p);
+                        direction = new_dir;
+                        break;
+                    }
+                }
+            }
+
+            // Окрасить границу
+            byte red = 0, green = 0, blue = 0;
+            switch (listBox1.SelectedIndex)
+            {
+                case 0:
+                    red = 255;
+                    green = blue = 0;
+                    break;
+                case 1:
+                    red = green = blue = 0;
+                    break;
+                case 2:
+                    blue = 255;
+                    red = green = 0;
+                    break;
+                case 3:
+                    green = 255;
+                    red = blue = 0;
+                    break;
+                case 4:
+                    red = green = blue = 255;
+                    break;
+            }
+            while (first != last)
+            {
+                var xx = first.Value.Item1;
+                var yy = first.Value.Item2;
+                //label4.Text += '(' + xx.ToString() + ',' + yy.ToString() + ") ";
+                rgbValues[bmpData.Stride * yy + 3 * xx + 2] = red;
+                rgbValues[bmpData.Stride * yy + 3 * xx + 1] = green;
+                rgbValues[bmpData.Stride * yy + 3 * xx] = blue;
+                first = first.Next;
+            }
+
+            /*bmp.UnlockBits(bmpData);
+            byte red = 0, green = 0, blue = 0;
+            switch (listBox1.SelectedIndex)
+            {
+                case 0:
+                    red = 255;
+                    green = blue = 0;
+                    break;
+                case 1:
+                    red = green = blue = 0;
+                    break;
+                case 2:
+                    blue = 255;
+                    red = green = 0;
+                    break;
+                case 3:
+                    green = 255;
+                    red = blue = 0;
+                    break;
+                case 4:
+                    red = green = blue = 255;
+                    break;
+            }
+            while (first != last)
+            {
+                var xx = first.Value.Item1;
+                var yy = first.Value.Item2;
+                label4.Text += '(' + xx.ToString() + ',' + yy.ToString() + ") ";
+                bmp.SetPixel(xx, yy, Color.FromArgb(red, green, blue));
+                first = first.Next;
+            }
+            pictureBox1.Refresh();*/
+
+            // Set every third value to 255. A 24bpp bitmap will look red.  
+            for (int counter = 0; counter < rgbValues.Length; counter += 3)
+            {
+                //rgbValues[counter+1] = 255;                                
+            }
+            // Copy the RGB values back to the bitmap
+            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+
+            // Unlock the bits.
+            bmp.UnlockBits(bmpData);
+
+            int i = 0;
+            for (int counter = 0; counter < rgbValues.Length; counter += 3)
+            {
+                bmp.SetPixel(i % bmp.Width, i / bmp.Width,
+                    Color.FromArgb(rgbValues[counter + 2], rgbValues[counter + 1], rgbValues[counter]));
+                i++;
+
+            }
+            pictureBox1.Refresh();
+    }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            label1.Text = openFileDialog1.FileName;
+            pictureBox1.Image = Bitmap.FromFile(label1.Text);
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = e.X.ToString();
+            textBox2.Text = e.Y.ToString();
+        }
+    }
+}
